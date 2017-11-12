0db9a9a9785fa2cb4ac6e5c66bdcdde283cb080c
Geoff St. Pierre <serundeputy@gmail.com>
2017-11-12 14:13:58 -0500

    Adding dusk testing framework.

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,17 +3,29 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use Tests\DuskTestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class ExampleTest extends DuskTestCase
 {
     /**
-     * A basic test example.
+     * Test that Login text is on the homepage.
      *
      * @return void
      */
-    public function testBasicTest()
-    {
-        $this->assertTrue(true);
-    }
+    // public function testBasicExample()
+    // {
+    //     // $this->visit('/')
+    //     //     ->see('Log in')
+    //     //     ->dontSee('Laravel 5');
+    //
+    //     $this->browse(function ($browser) {
+    //         $browser->visit('/')
+    //                 -> assertSee('Login')
+    //                 ->dontSee('Laravel 5');
+    //     });
+    // }
 }
