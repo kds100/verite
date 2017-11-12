28095dd1c61c8da01e2dd83d2b74c334a10a807f
Geoff St. Pierre <serundeputy@gmail.com>
2017-11-12 10:08:41 -0500

    Clean up routes file.

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'SurveyController@home')->name('home');
