18ffa54b8bc34b4c53446f85d4b4ecdaf296e1a5
Andrew Pasquale <andrew@elytra.net>
2017-11-15 17:51:11 -0500

    Include links to survey routes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,14 @@
 <ul class="collection with-header">
     <li class="collection-header">
         <h2 class="flow-text">Recent Surveys
-            <span style="float:right;">
+            <span style="float:right;">{{ link_to_route('new.survey', 'Create new') }}
             </span>
         </h2>
     </li>
     @forelse ($surveys as $survey)
       <li class="collection-item">
         <div>
-
+            {{ link_to_route('detail.survey', $survey->title, ['id'=>$survey->id])}}
             <a href="survey/view/{{ $survey->id }}" title="Take Survey" class="secondary-content"><i class="material-icons">send</i></a>
             <a href="survey/{{ $survey->id }}" title="Edit Survey" class="secondary-content"><i class="material-icons">edit</i></a>
             <a href="survey/answers/{{ $survey->id }}" title="View Survey Answers" class="secondary-content"><i class="material-icons">textsms</i></a>
